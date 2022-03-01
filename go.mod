889307264070fe5b68b3318c606d025fe38a9bbf
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-02-28 22:34:15 -0800

    CLI

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module github.com/Nv7-Github/bsharp
 
 go 1.17
 
-require github.com/bwmarrin/discordgo v0.23.3-0.20220227163103-4b615bec361d
+require (
+	github.com/alexflint/go-arg v1.4.3
+	github.com/bwmarrin/discordgo v0.23.3-0.20220227163103-4b615bec361d
+)
 
 require (
+	github.com/alexflint/go-scalar v1.1.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
--- main.go
+++ main.go
@@ -2,76 +2,60 @@ package main
 
 import (
 	_ "embed"
-	"math/rand"
+	"fmt"
 	"os"
-	"path/filepath"
 	"time"
 
 	"github.com/Nv7-Github/bsharp/backends/interpreter"
 	"github.com/Nv7-Github/bsharp/ir"
-	"github.com/Nv7-Github/bsharp/parser"
-	"github.com/Nv7-Github/bsharp/tokens"
+	"github.com/alexflint/go-arg"
 )
 
-//go:embed examples/main.bsp
-var code string
-
-type dirFS struct {
-	dir string
+type Run struct {
+	Files []string `arg:"positional,-i,--input" help:"input B++ program"`
 }
 
-func (d *dirFS) Parse(name string) (*parser.Parser, error) {
-	src, err := os.ReadFile(filepath.Join(d.dir, name))
-	if err != nil {
-		return nil, err
-	}
-	stream := tokens.NewTokenizer(tokens.NewStream(name, string(src)))
-	err = stream.Tokenize()
-	if err != nil {
-		return nil, err
-	}
-	parser := parser.NewParser(stream)
-
-	err = parser.Parse()
-	return parser, err
-}
-
-func extadd(a, b int) int {
-	return a + b
+type Args struct {
+	Run  *Run `arg:"subcommand:run" help:"run a B++ program"`
+	Time bool `help:"print timing for each stage" arg:"-t"`
 }
 
 func main() {
-	stream := tokens.NewStream("main.bsp", code)
-	tok := tokens.NewTokenizer(stream)
-	err := tok.Tokenize()
-	if err != nil {
-		panic(err)
-	}
+	args := Args{}
+	p := arg.MustParse(&args)
 
-	parser := parser.NewParser(tok)
-	err = parser.Parse()
-	if err != nil {
-		panic(err)
-	}
+	switch {
+	case args.Run != nil:
+		start := time.Now()
+		files := make(map[string]struct{}, len(args.Run.Files))
+		for _, f := range args.Run.Files {
+			files[f] = struct{}{}
+		}
+		// Build
+		fs := &dirFS{files}
+		v, err := fs.Parse(args.Run.Files[0])
+		if err != nil {
+			p.Fail(err.Error())
+		}
+		ir := ir.NewBuilder()
+		err = ir.Build(v, fs)
+		if err != nil {
+			p.Fail(err.Error())
+		}
 
-	// Extensions
-	ext, err := interpreter.NewExtension(extadd, "EXTADD")
-	if err != nil {
-		panic(err)
-	}
+		if args.Time {
+			fmt.Println("Built in", time.Since(start))
+		}
 
-	builder := ir.NewBuilder()
-	builder.AddExtension(ext.IRExtension())
-	err = builder.Build(parser, &dirFS{dir: "examples"})
-	if err != nil {
-		panic(err)
-	}
-
-	rand.Seed(time.Now().UnixNano())
-	interp := interpreter.NewInterpreter(builder.IR(), os.Stdout)
-	interp.AddExtension(ext)
-	err = interp.Run()
-	if err != nil {
-		panic(err)
+		// Run
+		start = time.Now()
+		interp := interpreter.NewInterpreter(ir.IR(), os.Stdout)
+		err = interp.Run()
+		if err != nil {
+			p.Fail(err.Error())
+		}
+		if args.Time {
+			fmt.Println("Ran in", time.Since(start))
+		}
 	}
 }
