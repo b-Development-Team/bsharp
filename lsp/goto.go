ff924dca9549f115ae10d3c2739ebdbe30c8c1d2
Nv7 <junk4Nv7@gmail.com>
2022-04-03 14:11:51 -0700

    Goto definition for variables

--- lsp/goto.go
+++ lsp/goto.go
@@ -9,6 +9,7 @@ import (
 )
 
 func definitionFn(context *glsp.Context, params *protocol.DefinitionParams) (interface{}, error) {
+	// Find token
 	doc, exists := Documents[params.TextDocument.URI]
 	if !exists {
 		return nil, nil
@@ -20,17 +21,48 @@ func definitionFn(context *glsp.Context, params *protocol.DefinitionParams) (int
 
 	// Check if function, get name
 	var name *string = nil
-	prev := doc.Tokens.Tokens[id-1]
 	tok := doc.Tokens.Tokens[id]
-	if prev.Typ == tokens.TokenTypeLBrack {
+	if matchPrev(id, doc, []TokenMatcher{TokMatchT(tokens.TokenTypeLBrack)}) {
 		name = &tok.Value
-	} else if prev.Typ == tokens.TokenTypeIdent && id > 1 {
-		prev2 := doc.Tokens.Tokens[id-2]
-		if prev2.Typ == tokens.TokenTypeLBrack {
-			name = &tok.Value
-		}
+	} else if matchPrev(id, doc, []TokenMatcher{TokMatchTV(tokens.TokenTypeIdent, "FN"), TokMatchT(tokens.TokenTypeLBrack)}) {
+		name = &tok.Value
 	}
 	if name == nil {
+		// Variable?
+		if matchPrev(id, doc, []TokenMatcher{TokMatchTV(tokens.TokenTypeIdent, "VAR"), TokMatchT(tokens.TokenTypeLBrack)}) {
+			// Variable
+			scope := GetScope(doc.IRCache, tok.Pos)
+			if scope == nil {
+				return nil, nil
+			}
+			for i := len(scope.Frames) - 1; i >= 0; i-- {
+				for _, v := range scope.Frames[i].Variables {
+					if doc.IRCache.Variables[v].Name == tok.Value {
+						// Get var pos
+						va := doc.IRCache.Variables[v]
+						id := tokID(protocol.Position{
+							Line:      uint32(va.Pos.Line),
+							Character: uint32(va.Pos.Char),
+						}, doc)
+						name := doc.Tokens.Tokens[id+2]
+
+						return protocol.Location{
+							URI: filepath.Join(RootURI, name.Pos.File),
+							Range: protocol.Range{
+								Start: protocol.Position{
+									Line:      uint32(name.Pos.Line),
+									Character: uint32(name.Pos.Char),
+								},
+								End: protocol.Position{
+									Line:      uint32(name.Pos.EndLine),
+									Character: uint32(name.Pos.EndChar),
+								},
+							},
+						}, nil
+					}
+				}
+			}
+		}
 		return nil, nil
 	}
 
