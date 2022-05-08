d1854c914b2f4c9647845e8214797a1090374ed9
Nv7 <junk4Nv7@gmail.com>
2022-05-08 15:29:16 -0700

    Add MATH

--- bsp/run.sh
+++ bsp/run.sh
@@ -1,1 +1,1 @@
-bsharp run bsp/main.bsp bsp/parser.bsp bsp/tokens.bsp bsp/types.bsp bsp/ir/ir.bsp bsp/ir/scope.bsp bsp/ir/build.bsp bsp/ir/stmts.bsp bsp/ir/basic.bsp
+bsharp run bsp/main.bsp bsp/parser.bsp bsp/tokens.bsp bsp/types.bsp bsp/ir/ir.bsp bsp/ir/scope.bsp bsp/ir/build.bsp bsp/ir/stmts.bsp bsp/ir/basic.bsp bsp/ir/ops.bsp
