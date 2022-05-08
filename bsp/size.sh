ebd9b6c84445ccf6c4854d5f65d0df3dfa8911ba
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-05-08 08:26:37 -0700

    Clean up output

--- bsp/size.sh
+++ bsp/size.sh
@@ -1,4 +1,4 @@
 # Analyze size of binary using bloaty
 clang -g examples/a.c -o a.out
-bloaty a.out -d symbols --debug-file=a.out.dSYM/Contents/Resources/DWARF/a.out -n 10000 > a.txt
+bloaty a.out -d symbols --debug-file=a.out.dSYM/Contents/Resources/DWARF/a.out -n 10000 --domain=file > a.txt
 rm -rf a.out.dSYM
