97c881363fa4bc04c4c95d36b529d416fe89922c
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-06-05 22:30:11 -0700

    Put in different program, fix bugs

--- bstar/run.sh
+++ bstar/run.sh
@@ -1,1 +1,1 @@
-bsharp run bstar/main.bsp bstar/parser.bsp bstar/interp.bsp bstar/funcs/vars.bsp bstar/funcs/funcs.bsp bstar/funcs/random.bsp
+bsharp run bstar/main.bsp bstar/parser.bsp bstar/interp.bsp bstar/funcs/vars.bsp bstar/funcs/funcs.bsp bstar/funcs/random.bsp -t
