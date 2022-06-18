7d714091f88b0ac43ea6ed52fe075bca14804d4c
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-06-18 15:15:37 -0700

    Make switch-case

--- backends/bstar/block.go
+++ backends/bstar/block.go
@@ -54,7 +54,47 @@ func (b *BStar) buildBlock(n *ir.BlockNode) (Node, error) {
 		bod = append([]Node{constNode("WHILE"), cond}, blockNode(false, append([]Node{constNode("BLOCK")}, append(bod, b.noPrintNode())...)...))
 		return blockNode(true, bod...), nil
 
+	case *ir.SwitchNode:
+		cnd, err := b.buildNode(bl.Value)
+		if err != nil {
+			return nil, err
+		}
+		end := b.noPrintNode()
+		if bl.Default != nil {
+			body, err := b.buildNodes(bl.Default.Block.(*ir.Default).Body)
+			if err != nil {
+				return nil, err
+			}
+			body = append(body, b.noPrintNode())
+			end = blockNode(false, append([]Node{constNode("BLOCK")}, body...)...)
+		}
+		bod, err := b.buildCase(cnd, bl.Cases, end)
+		if err != nil {
+			return nil, err
+		}
+		return bod, nil
+
 	default:
 		return nil, n.Pos().Error("unknown block: %T", bl)
 	}
+}
+
+func (b *BStar) buildCase(cnd Node, cs []*ir.BlockNode, end Node) (Node, error) {
+	ca := cs[0].Block.(*ir.Case)
+	cond, err := b.buildNode(ca.Value)
+	if err != nil {
+		return nil, err
+	}
+	body, err := b.buildNodes(ca.Body)
+	if err != nil {
+		return nil, err
+	}
+	body = append(body, b.noPrintNode())
+	if len(cs) > 1 {
+		end, err = b.buildCase(cnd, cs[1:], end)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return blockNode(false, constNode("IF"), blockNode(true, constNode("COMPARE"), cnd, constNode("=="), cond), blockNode(false, append([]Node{constNode("BLOCK")}, body...)...), end), nil
 }
