3589a51bdf57c47e0b5887d689575ca121380f21
Nv7 <50369270+Nv7-GitHub@users.noreply.github.com>
2023-02-02 22:37:16 +0000

    Fix setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 apt-get update
-apt-get install -y curl git
+apt-get install -y curl git build-essential
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
