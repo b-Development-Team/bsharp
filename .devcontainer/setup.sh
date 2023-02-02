6a365c25e15de9cbf54e325f25c7d02ba330c374
Nv7 <50369270+Nv7-GitHub@users.noreply.github.com>
2023-02-02 14:31:12 -0800

    Fix setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 apt-get update
-apt-get install curl
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup component add rustfmt
+apt-get install -y curl git
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
